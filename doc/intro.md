ab0941c6911452464439a23e4688874c1056e91a
Sebastian Bensusan <sbensu@gmail.com>
2017-11-04 16:00:10 -0700

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to crjdt
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/crjdt/core_test.clj
@@ -0,0 +1,7 @@
+(ns crjdt.core-test
+  (:require [clojure.test :refer :all]
+            [crjdt.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
